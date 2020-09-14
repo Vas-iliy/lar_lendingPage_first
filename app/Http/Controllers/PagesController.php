07269a3207dcb1fdb8d325dd40aa864576e28523
Vasiliy <kkolyasev1228@gmail.com>
2020-09-14 21:54:09 +0300

    pages controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Page;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function execute () {
+        if (view()->exists('admin.pages')) {
+            $pages = Page::all();
+            $title = 'Страницы';
+            return view('admin.pages', compact(['title', 'pages']));
+        }
+        else {
+            abort(404);
+        }
+    }
+}
